a1378c0e00a796193ae7e62e18c3ed54a76453ae
tobi_ade <tobi@grit.systems>
2018-09-17 22:59:01 +0100

    Implemented dropdown menu as clickable

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -5,37 +5,60 @@ import './sidebar.css';
 
 class Sidebar extends Component {
 
-    dropdown(id) {
-        let display = document.getElementById("id1").style
-        console.log(display)
-        // .style.display;
-
-        // if (display === "block") {
-        //     document.getElementById(id).style.display =  "none";
-        // } else {
-        //     document.getElementById(id).style.display =  "block";
-        // }
-    }
-
     render(){
     return(
         <div className="sidebar-nav">
             <div style={styles.sideDiv}>
                 <div style={styles.sideDivLink}><Link className="sidebar-link" to={"/home"}><span>Dashboard</span></Link></div>
-                <div style={styles.sideDivLink} className="dropdown"  onClick={this.dropdown("id1")}>
+                <div style={styles.sideDivLink} className="dropdown"  onClick={() => {
+                    let display = document.getElementById("id1");
+            
+                    if (display.style.display === "none") {
+                        display.style.display =  "block";
+                    } else {
+                        display.style.display =   "none";
+                    }
+                    console.log(display.style.display)
+                }}>
                     <Link className="sidebar-link" to={"#"}>Content Management</Link>
                     <div id="id1" className="dropdown-content"><Link className="sub-sidebar-link" to={"/partners"}><span>Merchant Partners</span></Link></div>
                 </div>
-                <div style={styles.sideDivLink} className="dropdown">
-                    <Link className="sidebar-link" to={"#"}><span>User Management</span></Link>
-                    <div className="dropdown-content"><Link className="sub-sidebar-link" to={"/users"}><span>Users</span></Link></div>
-                    <div className="dropdown-content"><Link className="sub-sidebar-link" to={"/admin_users"}><span>Admin Users</span></Link></div>
+                <div  style={styles.sideDivLink} className="dropdown" onClick={() => {
+                    let display = document.getElementById("id2");
+            
+                    if (display.style.display === "block") {
+                        display.style.display =  "none";
+                        document.getElementById("id3").style.display = "none";
+                    } else {
+                        display.style.display =  "block";
+                        document.getElementById("id3").style.display = "block";
+                    }
+                }}>
+                    
+                        <Link className="sidebar-link" to={"#"}><span>User Management</span></Link>
+                        <div id="id2" className="dropdown-content"><Link className="sub-sidebar-link" to={"/users"}><span>Users</span></Link></div>
+                        <div id="id3" className="dropdown-content"><Link className="sub-sidebar-link" to={"/admin_users"}><span>Admin Users</span></Link></div>
                 </div>
-                <div style={styles.sideDivLink} className="dropdown">
+                <br/>
+                <div style={styles.sideDivLink} className="dropdown" onClick={() => {
+                    let display = document.getElementById("id4");
+            
+                    if (display.style.display === "block") {
+                        display.style.display =  "none";
+                        document.getElementById("id5").style.display = "none";
+                        document.getElementById("id6").style.display = "none";
+                    } else {
+                        display.style.display =  "block";
+                        document.getElementById("id5").style.display = "block";
+                        document.getElementById("id6").style.display = "block";                    
+                    }
+                }}>
                     <Link className="sidebar-link" to={"#"}><span>Reporting</span></Link>
-                    <div className="dropdown-content"><Link className="sub-sidebar-link" to={"/portal_report"}><span>Portal Report</span></Link></div>
-                    <div className="dropdown-content"><Link className="sub-sidebar-link" to={"/redemption_report"}><span>Redemption Report</span></Link></div>
-                    <div className="dropdown-content"><Link className="sub-sidebar-link" to={"/bi_report"}><span>BI</span></Link></div>
+                    <div id="id3">
+                        <div id="id4" className="dropdown-content"><Link className="sub-sidebar-link" to={"/portal_report"}><span>Portal Report</span></Link></div>
+                        <div id="id5" className="dropdown-content"><Link className="sub-sidebar-link" to={"/redemption_report"}><span>Redemption Report</span></Link></div>
+                        <div id="id6" className="dropdown-content"><Link className="sub-sidebar-link" to={"/bi_report"}><span>BI</span></Link></div>
+                    </div>
                 </div>
                 <div style={styles.sideDivLink}><Link className="sidebar-link" to={"/settings"}><span>System Settings</span></Link></div>
                 <div style={{ height:"150px" }}></div>
