34aedb075c880b05b3a9b9299fa99c42744c9368
tobi_ade <tobi@grit.systems>
2018-12-18 15:42:52 +0100

    Updated login component

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -10,8 +10,9 @@ class Login extends Component{
 
     componentDidMount(){
         let body = document.body.style
-        body.backgroundImage = "url('../../assets/bg.jpg')";
-        body.backgroundRepeat = "no-repeat";
+        body.background = "url('../../assets/bg.jpg') no-repeat center center fixed";
+        body.backgroundSize = "cover"
+        body.webkitBackgroundSize = "cover"
         body.width = "100%";
         body.height = "auto";
     }
@@ -35,24 +36,34 @@ class Login extends Component{
                 this.state.toHome ?
                 (<Redirect to={"/home"} />)
                 :
-            <div className="login-bg">
                 <div className="login-card">
+                    <div className="login-header">
+                        <div className="sign-in">Sign In</div>
+                        <div><img alt="UBA" src={logo} className="uba-logo-login"/></div>
+                    </div>
                     <form onSubmit={this.onsubmit} className="login-form">
-                        <div className="login-header">
-                            <div className="sign-in">Sign In</div>
-                            <div className="sign-in-logo"><img alt="UBA" src={logo} className="uba-logo"/></div>
-                        </div>
-                        <span className="login-email">Email</span>
-                        <input className="login-input" type={"email"} name="email" required onInput={val => {console.log(val.target.value)}}></input>
-                        <span className="login-password">Password</span>
-                        <input className="login-input" type={"password"} name="password" required onInput={val => {console.log(val.target.value)}}></input>
-                        <Link to={"/"} className="forgot-password">Forgot Your Password</Link>
-                        <input type="submit" className="login-button login-text" value="Login"></input>
-                        <div className="no-account">Don't have an account?</div>
+                        
+                        <label for={"email"}>Email</label>
+                        <input type={"email"} name="email" required onInput={val => {console.log(val.target.value)}}/>
+
+                        <label for={"password"}>Password</label>
+                        <input type={"password"} name="password" required onInput={val => {console.log(val.target.value)}}></input>
+
+                        <Link to={"/"} className="forgot-password">Forgot Your Password?</Link>
+
+                        <button>
+                            Login<i style={{ float: "right" }} className="fa fa-angle-right"></i>
+                        </button>
                     </form>
-                    <button className="request-access-button"><span className="request-access-text">Request for Access</span></button>
+                    
+                    <div style={{display: 'grid'}}>
+                        <div className="no-account">Don't have an account?</div>
+                        <button className="request-access-button">
+                            <span className="request-access-text">Request for Access</span>
+                            <i style={{ float: "right" }} className="fa fa-angle-right"></i>
+                        </button>
+                    </div>
                 </div>
-            </div>
             }
             </div>
         )
