16e5d0546488a24209d20de81980407a023f2f81
tobi_ade <tobi@grit.systems>
2018-12-21 20:01:22 +0100

    Responsiveness 80% complete

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -6,6 +6,44 @@ import Header from '../header/header';
 class Home extends Component{
     d = new Date()
     
+    componentDidMount() {
+        let cv = ["canvas1", "canvas2", "canvas3", "canvas4"];
+        let col = ["rgb(226, 17, 17)", "rgb(96, 226, 113)", "rgb(65, 156, 144)", "rgb(141, 18, 145)"]
+        
+        // for (let i = 0; i < cv.length; ++i){
+        //     let ctx = document.getElementById(cv[i]);
+        //     ctx = ctx.getContext("2d");
+        //     ctx.beginPath();
+        //     ctx.moveTo(20, 100)
+        //     ctx.quadraticCurveTo(40, 100, 40, 90);
+        //     ctx.quadraticCurveTo(40, 80, 50, 80);
+        //     ctx.lineTo(65, 105);
+        //     ctx.quadraticCurveTo(75, 110, 120, 60)
+        //     ctx.quadraticCurveTo(140, 65, 140, 100)
+        //     ctx.quadraticCurveTo(140, 160, 200, 120)
+        //     ctx.strokeStyle = col[i];
+        //     ctx.lineWidth = 3;
+        //     ctx.stroke();
+        // }
+        
+        // let sumcv = document.getElementById("summary-canvas");
+        // let ctx = sumcv.getContext("2d");
+        // ctx.beginPath();
+        // ctx.moveTo(0, 80)
+        // ctx.lineTo(100, 0);
+        // ctx.lineTo(200, 50);
+        // ctx.lineTo(300, 10);
+        // ctx.moveTo(300, 10);
+        // ctx.lineTo(400, 100);
+        // ctx.lineTo(500, 120);
+        // ctx.lineTo(586, 25);
+        // ctx.fillStyle="red";
+        // ctx.fill()
+        // ctx.strokeStyle =  "grey"
+        // ctx.stroke();
+    }
+
+
     render(){
         return(
             <div>
@@ -35,22 +73,31 @@ class Home extends Component{
                             <div className="customers">
                                 <div className="values">1,234</div>
                                 <div className="description">Registered Customers</div>
-                                <div className="charts"></div>
+                                <div className="charts">
+                                    <canvas className="canvas" id="canvas1" ref="canvas1"/>
+
+                                </div>
                             </div>
                             <div className="redemption-value ">
                                 <div className="values">1,234</div>
                                 <div className="description">Value of Redemption</div>
-                                <div className="charts"></div>
+                                <div className="charts">
+                                    <canvas className="canvas" id="canvas2" ref="canvas2"/>
+                                </div>
                             </div>
                             <div className="points-redeemed">
                                 <div className="values">3,456</div>
                                 <div className="description">Points Redeemed</div>
-                                <div className="charts"></div>
+                                <div className="charts">
+                                    <canvas className="canvas" id="canvas3" ref="canvas3"/>
+                                </div>
                             </div>
                             <div className="cash-spend">
                                 <div className="values">3,456,789</div>
                                 <div className="description">Cash Spend(Debit Card)</div>
-                                <div className="charts"></div>                            
+                                <div className="charts">
+                                    <canvas className="canvas" id="canvas4" ref="canvas4"/>
+                                </div>
                             </div>
                         </div>
                         <div className="summary">
@@ -61,12 +108,12 @@ class Home extends Component{
                                     type={"button"}
                                     style={{ gridColumn: 6, fontSize: "20px", height: "40px", color: "white", borderRadius:"25px", border: "none", backgroundColor: "#C82B1D"}}
                                     onClick={()=>{
-                                        let month1 = document.getElementById("#id1")
-                                        let month2 = document.getElementById("#id2")
-                                        month1.style.color = "white"
-                                        month1.style.backgroundColor = "#C82B1D"
-                                        month2.style.color = "black"
-                                        month2.style.backgroundColor = "white"
+                                        let id1 = document.getElementById("#id1")
+                                        let id2 = document.getElementById("#id2")
+                                        id1.style.color = "white"
+                                        id1.style.backgroundColor = "#C82B1D"
+                                        id2.style.color = "black"
+                                        id2.style.backgroundColor = "white"
                                     }}>
                                     Month
                                     </button>
@@ -74,12 +121,12 @@ class Home extends Component{
                                     id={"#id2"}
                                     style={{gridColumn:7, fontSize: "20px", height: "40px", border: "none", borderRadius:"25px", backgroundColor: "white" }}
                                     onClick={()=>{
-                                        let month1 = document.getElementById("#id1")
-                                        let month2 = document.getElementById("#id2")
-                                        month2.style.color="white"
-                                        month2.style.backgroundColor = "#C82B1D"
-                                        month1.style.color="black"
-                                        month1.style.backgroundColor = "white"
+                                        let id1 = document.getElementById("#id1")
+                                        let id2 = document.getElementById("#id2")
+                                        id2.style.color="white"
+                                        id2.style.backgroundColor = "#C82B1D"
+                                        id1.style.color="black"
+                                        id1.style.backgroundColor = "white"
                                     }}
                                     >
                                     All Time
@@ -105,7 +152,8 @@ class Home extends Component{
                                     <div className="summary-value">123456789</div>
                                 </div>
                             </div>
-                            <div className="summary-chart"></div>
+                            <div className="summary-chart">
+                                <canvas width={"680"} id="summary-canvas" ref="summarycanvas" style={{paddingTop: "50px"}}></canvas></div>
                         </div>
                     </div>
                 </div>
