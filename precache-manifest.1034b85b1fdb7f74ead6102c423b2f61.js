288e5db35074ba05c771c13cbba717fdf497368a
tobi_ade <tobi@grit.systems>
2018-12-27 03:54:31 +0100

    Updates

---
+++ precache-manifest.1034b85b1fdb7f74ead6102c423b2f61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a4227edd6f5f103b8334ce9309b3d0c",
+    "url": "/portal/static/media/logo.5a4227ed.png"
+  },
+  {
+    "revision": "9d0029aa1b9d6035a90e",
+    "url": "/portal/static/js/runtime~main.9d0029aa.js"
+  },
+  {
+    "revision": "f1ebdabc2472508c3965",
+    "url": "/portal/static/js/main.f1ebdabc.chunk.js"
+  },
+  {
+    "revision": "fbc82d83022ea86c287a",
+    "url": "/portal/static/js/1.fbc82d83.chunk.js"
+  },
+  {
+    "revision": "f1ebdabc2472508c3965",
+    "url": "/portal/static/css/main.cf8d6836.chunk.css"
+  },
+  {
+    "revision": "bd46b32b2c153e8c66e3f3503e08eb5a",
+    "url": "/portal/index.html"
+  }
+];
